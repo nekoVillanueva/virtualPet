db42ab6d9a2c88db8f0203242bc45bc1fe16709a
nekoVillanueva <nekovillan@gmail.com>
2018-09-26 10:14:51 -0400

    pet

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+
+        static void Main()
+        {
+
+            string answer;
+            Virtual_Pet pet = new Virtual_Pet();
+
+
+            Console.WriteLine("Welcome to Jurassic Park");
+            Console.WriteLine("What would you like to call your new dinosaur?");
+            pet.GetName = Console.ReadLine().ToLower();
+
+            Console.WriteLine(pet.GetName + " the dinosaur needs TLC so make sure you take care of him");
+            while (pet.GetHealth > 0 && pet.GetThirst > 0 && pet.GetHunger > 0)
+            {
+
+                Console.WriteLine("{0} health is: {1}", pet.GetName, pet.GetHealth);
+                Console.WriteLine("hunger: {0}", pet.GetHunger);
+                Console.WriteLine("thirst:{0}", pet.GetThirst);
+                Console.WriteLine("What would you like to do for your new dinosaur ,type feed to give pet food or drink to give pet water .");
+                answer = Console.ReadLine().ToLower();
+                pet.StatusChange(answer);
+
+
+
+
+            }
+
+            if (pet.GetHealth <= 0)
+            {
+                Console.WriteLine("{0} has been hit by an asteroid and has passed away", pet.GetName);
+            }
+            else if (pet.GetHunger <= 0)
+            {
+                Console.WriteLine("{0} has eaten you so may you R.I.P", pet.GetName);
+            }
+            else
+            {
+                Console.WriteLine("{0} has shriveled up from dehydration and decided to get his water from your blood", pet.GetName);
+            }
+
+            Console.WriteLine("Would you like to return to Jurassic Park? Enter yes if youd like to return or no to exit park.");
+            answer = Console.ReadLine();
+            if (answer == "yes")
+            {
+                Main();
+            }
+            else
+            {
+                return;
+
+            }
+        }
+    }
+}
+//public void Menu()
+//{
+//    Console.WriteLine(pet.GetName+ " the dinosaur");
+//    Console.WriteLine()
+
+//}
---
+++ VirtualPet/Virtual Pet.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Virtual_Pet
+    {
+        //fields
+        private string name;
+        private int health=100;
+        private int hunger=10;
+        private int thirst=10;
+
+        //Properties
+        public string GetName
+        {
+
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int GetHealth
+        {
+            get { return this.health; }
+            set { this.health = value; }
+        }
+        public int GetHunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+        public int GetThirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        //Constructors
+        public Virtual_Pet()
+        {
+
+        }
+        public Virtual_Pet(int health,int hunger,int thirst)
+        {
+            this.GetHealth = health;
+            this.GetHunger = hunger;
+            this.GetThirst = thirst;
+        }
+
+        //Methods
+        public void HealthChange()
+        {
+            if ((GetHunger<=6)&&(GetThirst<=6))
+            {
+                GetHealth = GetHealth - 10;
+            }
+        }
+        public void StatusChange(string answer)
+        {
+            if(answer=="feed")
+            {
+                HungerChange();
+                Tic();
+            }
+            else if(answer=="drink")
+            {
+                ThirstChange();
+                Tic();
+            }
+            else
+            {
+                Console.WriteLine("Please choose valid entry before your dinosaur consumes you");
+
+            }
+        }
+        public void HungerChange()
+        {
+            GetHunger = GetHunger + 2;
+        }
+        public void ThirstChange()
+        {
+            GetThirst = GetThirst + 4;
+        }
+        public void Vet()
+        {
+            GetHealth = 100;
+            Console.WriteLine("{0} health has been restored to 100 but your vet bills were not cheap", GetName);
+
+        }
+        public void Tic()
+        {
+            GetHealth = GetHealth - 20;
+            GetHunger = GetHunger - 2;
+            GetThirst = GetThirst - 2;
+            HealthChange();
+            if (GetHealth <= 40)
+            {
+                
+                Console.WriteLine("Your dinosaur is looking sickly would you like go to the vet?");
+                string input = Console.ReadLine().ToLower();
+                if (input == "yes")
+                {
+                    Vet();
+                }
+                else if (GetHealth <= 20)
+
+                {
+                    Console.WriteLine("Your dinosaur is having dreams of asteroids and coughing up blood.He isnt going to make  it.");
+                    
+                    return;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
